5ec536b0a6d1f09ccf7ff342e7ec6af4d2d05918
Diya Kithani <diyakithani.2003@gmail.com>
2024-08-11 10:14:54 -0400

    NavBar! :)

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Create a React element
-const h = React.createElement("h1", { className: "title" }, "heading");
+const Title = () => {
+  return (
+    <>
+      <img
+        alt="this is the brand's logo"
+        className="logo"
+        src="https://feastly.nl/wp-content/uploads/2024/04/Logo.png"
+      />
+
+      <h1>Feastly - The end to your hunger! :) </h1>
+    </>
+  );
+};
+
+const Navitems = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact Us</li>
+    </ul>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <Navitems></Navitems>
+      </div>
+    </div>
+  );
+};
 
 // Get the root DOM node
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(h);
+root.render(<AppLayout />);
